54593a60378399e861ad84e6bfbe6abda3c7bffc
Apple <apple@Apples-MacBook-Pro.local>
2022-02-10 23:33:58 +0530

    commands working

--- commands.js
+++ commands.js
@@ -5,6 +5,8 @@ const  { Routes } =  require('discord-api-types/v9');
 const  { config } =  require("dotenv");
 config({path: `${__dirname}/.env.${process.env.NODE_ENV}`});
 
+const {BOT_TOKEN,CLIENT_ID,GUILD_ID}= process.env;
+
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
@@ -12,10 +14,9 @@ const commands = [
 ]
 	.map(command => command.toJSON());
 
-const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
+const rest = new REST({ version: '9' }).setToken(BOT_TOKEN);
 
 rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
 
-	
